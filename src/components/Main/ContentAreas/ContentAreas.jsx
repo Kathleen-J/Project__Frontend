d3f3b5dcad34cb6bd6355083b3b859df917a9bc4
Kathleen-J <gekaterinasergeevna@mail.ru>
2022-06-26 22:22:55 +0500

    style refactoring

--- src/components/Main/ContentAreas/ContentAreas.jsx
+++ src/components/Main/ContentAreas/ContentAreas.jsx
@@ -2,60 +2,75 @@ import css from "./ContentAreas.module.css";
 
 const ContentAreas = () => {
   return (
-    <div>
-      <div className={css.content_areas}>
-        <div className={css.areas}>Направления</div>
-        <div className={css.all_areas}>
-          <div className={css.area}>
-            <p className={`${css.name} ${css.development}`}>Программирование</p>
-            <div className={css.lineDecorator}></div>
-            <p className={css.info}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              qui libero fuga rerum quaerat explicabo iure beatae quas eum, ab
-              magnam accusamus, quae ratione fugiat maiores? Libero id sapiente
-              ea. Lorem ipum dolor sit amet consectetur adipisicing elit.
-              Eligendi qui libero fuga rerum quaerat explicabo iure beatae quas
-              eum, ab magnam accusamus, quae ratione fugiat maiores? Libero id
-              sapiente ea explicabo iure beatae quas eum, ab magnam accusamus,
-              quae ratione fugiat maiores? Libero id sapiente ea. Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Eligendi qui libero
-              fuga rerum quaerat explicabo iure beatae quas eum
-            </p>
-          </div>
-          <div className={css.area}>
-            <p className={`${css.name} ${css.design}`}>Дизайн</p>
-            <div className={css.lineDecorator}></div>
-            <p className={css.info}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              qui libero fuga rerum quaerat explicabo iure beatae quas eum, ab
-              magnam accusamus, quae ratione fugiat maiores? Libero id sapiente
-              ea. Lorem ipum dolor sit amet consectetur adipisicing elit.
-              Eligendi qui libero fuga rerum quaerat explicabo iure beatae quas
-              eum, ab magnam accusamus, quae ratione fugiat maiores? Libero id
-              sapiente ea explicabo iure beatae quas eum, ab magnam accusamus,
-              quae ratione fugiat maiores? Libero id sapiente ea. Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Eligendi qui libero
-              fuga rerum quaerat explicabo iure beatae quas eum
-            </p>
-          </div>
-          <div className={css.area}>
-            <p className={`${css.name} ${css.languages}`}>Иностранные языки</p>
-            <div className={css.lineDecorator}></div>
-            <p className={css.info}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              qui libero fuga rerum quaerat explicabo iure beatae quas eum, ab
-              magnam accusamus, quae ratione fugiat maiores? Libero id sapiente
-              ea. Lorem ipum dolor sit amet consectetur adipisicing elit.
-              Eligendi qui libero fuga rerum quaerat explicabo iure beatae quas
-              eum, ab magnam accusamus, quae ratione fugiat maiores? Libero id
-              sapiente ea explicabo iure beatae quas eum, ab magnam accusamus,
-              quae ratione fugiat maiores? Libero id sapiente ea. Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Eligendi qui libero
-              fuga rerum quaerat explicabo iure beatae quas eum
-            </p>
-          </div>
+    <div className={css.content_areas}>
+
+        <div className={css.areas_content}>
+          <div className={css.lineDecorator}></div>
+          <div className={css.areas}>Направления</div>
+          <div className={css.lineDecorator}></div>
         </div>
-      </div>
+
+        <div className={css.all_areas}>
+
+          <div className={css.development_area}>
+            <div className={css.area}>
+              <p className={`${css.name} ${css.development}`}>Программирование</p>
+              <div className={css.lineDecorator}></div>
+              <p className={css.info}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                qui libero fuga rerum quaerat explicabo iure beatae quas eum, ab
+                magnam accusamus, quae ratione fugiat maiores? Libero id sapiente
+                ea. Lorem ipum dolor sit amet consectetur adipisicing elit.
+                Eligendi qui libero fuga rerum quaerat explicabo iure beatae quas
+                eum, ab magnam accusamus, quae ratione fugiat maiores? Libero id
+                sapiente ea explicabo iure beatae quas eum, ab magnam accusamus,
+                quae ratione fugiat maiores? Libero id sapiente ea. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Eligendi qui libero
+                fuga rerum quaerat explicabo iure beatae quas eum
+              </p>
+            </div>
+          </div>
+
+          <div className={css.design_area}>            
+            <div className={css.area}>
+              <p className={`${css.name} ${css.design}`}>Дизайн</p>
+              <div className={css.lineDecorator}></div>
+              <p className={css.info}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                qui libero fuga rerum quaerat explicabo iure beatae quas eum, ab
+                magnam accusamus, quae ratione fugiat maiores? Libero id sapiente
+                ea. Lorem ipum dolor sit amet consectetur adipisicing elit.
+                Eligendi qui libero fuga rerum quaerat explicabo iure beatae quas
+                eum, ab magnam accusamus, quae ratione fugiat maiores? Libero id
+                sapiente ea explicabo iure beatae quas eum, ab magnam accusamus,
+                quae ratione fugiat maiores? Libero id sapiente ea. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Eligendi qui libero
+                fuga rerum quaerat explicabo iure beatae quas eum
+              </p>
+            </div>
+          </div>
+          
+          <div className={css.languages_area}>            
+            <div className={css.area}>
+              <p className={`${css.name} ${css.languages}`}>Иностранные языки</p>
+              <div className={css.lineDecorator}></div>
+              <p className={css.info}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                qui libero fuga rerum quaerat explicabo iure beatae quas eum, ab
+                magnam accusamus, quae ratione fugiat maiores? Libero id sapiente
+                ea. Lorem ipum dolor sit amet consectetur adipisicing elit.
+                Eligendi qui libero fuga rerum quaerat explicabo iure beatae quas
+                eum, ab magnam accusamus, quae ratione fugiat maiores? Libero id
+                sapiente ea explicabo iure beatae quas eum, ab magnam accusamus,
+                quae ratione fugiat maiores? Libero id sapiente ea. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Eligendi qui libero
+                fuga rerum quaerat explicabo iure beatae quas eum
+              </p>
+            </div>
+          </div>
+
+        </div> 
+
     </div>
   );
 };
--- src/components/Main/Title/Title.jsx
+++ src/components/Main/Title/Title.jsx
@@ -5,10 +5,12 @@ const Title = () => {
     <div className={css.title}>
       <div className={css.education_left}>
         <p className={css.education_online}>ОНЛАЙН ОБУЧЕНИЕ</p>
-        <p className={css.education}>Учитесь</p>
-        <p className={`${css.education} ${css.description_one}`}>в <span> удобное </span> для</p>
-        <p className={`${css.education} ${css.description_two}`}>вас <span> время</span></p>
-        <p className={css.skillford}>вместе со SkillFox</p>
+        <div className={css.description}>          
+          <p className={`${css.education} ${css.description_three}`}>Учитесь</p>
+          <p className={`${css.education} ${css.description_one}`}>в <span> удобное </span> для</p>
+          <p className={`${css.education} ${css.description_two}`}>вас <span> время</span></p>
+        </div>
+        <p className={`${css.skillfox} ${css.description}`}>вместе со SkillFox</p>
       </div>
       <div className={css.education_right}>
         <img src={require('./fox.png')} alt="foxy" className={css.fox}/>
